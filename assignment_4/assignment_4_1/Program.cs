e99b41be727f9f4f9952e1d6e4190de62ce32004
Exdenl <15488474+Miao_Yu_0306@user.noreply.gitee.com>
2025-03-19 22:41:47 +0800

    My HW_4

---
+++ assignment_4/assignment_4_1/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+
+public class GenericArray<T>
+{
+    private T[] array;
+    public GenericArray(int size)
+    {
+        array = new T[size];
+    }
+
+    public T getItem(int index)
+    {
+        return array[index];
+    }
+
+    public void setItem(int index, T value)
+    {
+        array[index] = value;
+    }
+    public void ForEach(Action<T> action)
+    {
+        foreach (var item in array)
+        {
+            action(item);
+        }
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("请输入数组的大小：");
+        int size = int.Parse(Console.ReadLine());
+
+        GenericArray<int> array = new GenericArray<int>(size);
+
+        Console.WriteLine($"请输入 {size} 个整数：");
+        for (int i = 0; i < size; i++)
+        {
+            Console.Write($"请输入第 {i + 1} 个元素: ");
+            int value = int.Parse(Console.ReadLine());
+            array.setItem(i, value);
+        }
+
+        Console.WriteLine("\n数组元素:");
+        array.ForEach(value => Console.WriteLine(value));
+
+        int max = int.MinValue;
+        array.ForEach(value => {
+            if (value > max)
+            {
+                max = value;
+            }
+        });
+        Console.WriteLine($"\n数组中的最大值: {max}");
+
+        int min = int.MaxValue;
+        array.ForEach(value => {
+            if (value < min)
+            {
+                min = value;
+            }
+        });
+        Console.WriteLine($"\n数组中的最小值: {min}");
+
+        int sum = 0;
+        array.ForEach(value => sum += value);
+        Console.WriteLine($"\n数组元素的和: {sum}");
+    }
+}
---
+++ assignment_4/assignment_4_2/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Timers;
+
+public class AlarmClock
+{
+    public event EventHandler Tick;
+    public event EventHandler Alarm;
+
+    private System.Timers.Timer timer;
+    private DateTime alarmTime;
+
+    public AlarmClock()
+    {
+        timer = new System.Timers.Timer(1000);
+        timer.Elapsed += OnTick;
+    }
+
+    public void SetAlarm(DateTime alarmTime)
+    {
+        this.alarmTime = alarmTime;
+    }
+
+    public void Start()
+    {
+        timer.Start();
+        Console.WriteLine("闹钟开始走了！");
+    }
+
+    public void Stop()
+    {
+        timer.Stop();
+        Console.WriteLine("闹钟已停止。");
+    }
+
+    private void OnTick(object sender, ElapsedEventArgs e)
+    {
+        Tick?.Invoke(this, EventArgs.Empty);
+
+        if (DateTime.Now >= alarmTime)
+        {
+            Alarm?.Invoke(this, EventArgs.Empty);
+            Stop();
+        }
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        AlarmClock clock = new AlarmClock();
+
+        clock.Tick += (sender, e) =>
+        {
+            Console.WriteLine("嘀嗒...");
+        };
+
+        clock.Alarm += (sender, e) =>
+        {
+            Console.WriteLine("响铃！时间到！");
+        };
+
+        DateTime alarmTime = DateTime.Now.AddSeconds(5);
+        clock.SetAlarm(alarmTime);
+
+        clock.Start();
+
+        Console.WriteLine($"闹钟时间已设置为: {alarmTime}");
+        Console.WriteLine("按任意键停止闹钟...");
+        Console.ReadKey();
+    }
+}
